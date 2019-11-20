e137bcd8ef9e3ae95a50a45dec7d8bb831662e4c
liuyinhsiang <liuyinhsiang@gmail.com>
2019-11-21 07:35:21 +1100

    Updates

---
+++ precache-manifest.4e967ed87877b09463fd25b65b3dae11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed30f7e1ea98a6227c71dd46ce6fd60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4083a7c7e70630ee8cce",
+    "url": "/monsters-rolodex/static/css/main.79cf9313.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4083a7c7e70630ee8cce",
+    "url": "/monsters-rolodex/static/js/main.c5f71ac3.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
